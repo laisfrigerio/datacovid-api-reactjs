b2f9f941ecec5b84baa4d0a66979d1fa78f344b2
Lucas Lima <lucas.alima@souunit.com.br>
2020-07-30 15:26:47 -0300

    build

---
+++ build/precache-manifest.25affd788b9b6f9a2ce71a1289e6114d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f314c7efc91b8ed78d5edc0b83cb2c79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53e716975b1afe187af0",
+    "url": "/static/css/main.9acc010b.chunk.css"
+  },
+  {
+    "revision": "eb7d8adc3b0c97dbe8cf",
+    "url": "/static/js/2.aa9847c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aa9847c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e716975b1afe187af0",
+    "url": "/static/js/main.59c3de24.chunk.js"
+  },
+  {
+    "revision": "d2413320a277bb2c659c",
+    "url": "/static/js/runtime-main.c14436a3.js"
+  }
+]);
